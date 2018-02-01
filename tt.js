f0145ca73213306cfdd80f99b98686dd62845190
Unknown <edeal100@west-mec.org>
2018-02-01 15:28:28 -0700

    Started Exercise
    We stored data in arrays, declared and initialized arrays, and accessed element information.

---
+++ tt.js
@@ -0,0 +1,14 @@
+/*
+Tipton Turbines
+Calendar web page
+Author: Embery Deal
+Date:   2.1.18
+
+Filename: tt.js
+*/
+
+var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wedneday", "Thursday", "Friday", "Saturday"];
+
+var opponents = ["Lightning", "Combines", "Combines", "Lightning", "Lightning", "Lightning", "Lightning", "Barn Raisers", "Barn Raisers", "Barn Raisers", "Sodbusters", "Sodbusters", "Sodbusters", "Sodbusters", "(off)", "River Riders", "River Riders", "River Riders", "Big Dippers", "Big Dippers", "Big Dippers", "(off)", "Sodbusters", "Sodbusters", "Sodbusters", "Combines", "Combines", "Combines", "(off)", "(off)"];
+
+var gameLocation = ["away", "away", "away", "away", "home", "home", "home", "home", "home", "home", "home", "away", "away", "away", "away", "", "away", "away", "away", "away", "away", "away", "", "home", "home", "home", "home", "home", "home", "", ""];
